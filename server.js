954926685efb2f7505c4709f803c412fa400a17a
Samuel <samuelagih16@gmail.com>
2026-07-27 18:46:43 +0100

    Add root route

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.json({
 app.use(cookieParser());
 
 app.get('/api/health', (req, res) => res.json({ ok: true }));
+app.get('/', (req, res) => res.send('Loopline API is running 🚀'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/payments', paymentRoutes);
